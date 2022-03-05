c4062e3ee0a54532bbdc765f38554ca7aadc543a
Artur Rozdaibida <makechansongreateagain@gmail.com>
2022-03-05 22:46:58 +0200

    Add project files.

---
+++ yor-auth-api/Model/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace yor_auth_api.Model
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ yor-auth-api/Model/User.cs
@@ -0,0 +1,19 @@
+﻿namespace yor_auth_api.Model
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+    }
+}
